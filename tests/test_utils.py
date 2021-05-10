826eaa81f6473590e6af63f3d2512a70358c8fa2
Baihan Lin <doerlbh@gmail.com>
2021-05-09 20:29:25 -0400

    [FEAT] add tests for utils

---
+++ tests/test_utils.py
@@ -0,0 +1,15 @@
+"""Test for the utils"""
+from unittest import TestCase
+import numpy as np
+
+from banditzoo import utils
+
+class TestUtils(TestCase):
+    def test_the_default_obj_works(self):
+        rewards = [1,2]
+        w = 0.5
+        r = utils.default_obj(rewards, w)
+        expected_r = w * rewards[0] + (1 - w) / rewards[1]
+        self.assertEqual(expected_r, expected_r)
+
+
